b549e3e6d4121a9244da86d977240dc29c6415cc
priyanshuxch <priyanshuxch@gmail.com>
2026-02-09 09:25:34 +0530

    Add state variables and declare three methods for generating password string, actual password and resetting it

--- App.tsx
+++ App.tsx
@@ -1,7 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+
+import * as Yup from 'yup'
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+  .min(4, 'Should be min of 4 characters')
+  .max(16, 'Should be max of 16 characters')
+  .required('Length is required'  )
+})
 
 const App = () => {
+  const [password, setPassword] = useState('')
+  const [isPassGenerated, setIsPassGenerated] = useState(false)
+
+  const [lowerCase, setLowerCase] = useState(true)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [symbols, setSymbols] = useState(false)
+
+  const generatePasswordString = (passwordLength: number) => {}
+
+  const createPassword = (characters: string, passwordLength: number) => {}
+
+  const resetPasswordState = () => {}
+
   return (
     <View>
       <Text>App</Text>
